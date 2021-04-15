246fdfa17fa13ea25208844a262702ce202245bd
Matthew Ito <mito7@hawaii.edu>
2021-04-14 17:35:31 -0700

    ListProfiles and ListCompanies
    Created a ListProfiles and ListCompanies page which displays a temporary collection of UserProfiles and CompanyProfiles

---
+++ app/imports/ui/components/CompanyProfile.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class CompanyProfile extends React.Component {
+  render() {
+    return (
+        <Card>
+          <Image src={this.props.companyprofile.image} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{this.props.companyprofile.name}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{this.props.companyprofile.address}</span>
+            </Card.Meta>
+            <Card.Description>
+              {this.props.companyprofile.description}
+            </Card.Description>
+          </Card.Content>
+        </Card>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+CompanyProfile.propTypes = {
+  companyprofile: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    address: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(CompanyProfile);
